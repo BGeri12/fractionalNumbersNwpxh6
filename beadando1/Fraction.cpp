d54808a5becfb0fee4a8fb5a7246da1e9e3ff290
gerib2001 <gerib2001@gmail.com>
2026-02-27 18:44:53 +0100

    Feat: avoiding division by zero

--- beadando1/Fraction.cpp
+++ beadando1/Fraction.cpp
@@ -1,2 +1,15 @@
 #include "Fraction.h"
+#include <stdexcept>
 
+Fraction::Fraction(const int numerator, const int denominator)
+	: numerator(numerator)
+	, denominator(denominator)
+{
+	if (this->denominator == 0)
+		throw std::invalid_argument("A nevezõ nem lehet nulla!");
+
+	if (this->denominator < 0){
+		this->numerator *= -1;
+		this->denominator *= -1;
+	}
+}
